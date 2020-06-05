ed19d41143bb0a1f4b5e55c56420786ff2827bc1
Minho Choi <minhochoi.official@gmail.com>
2020-06-05 23:33:10 +0900

    Updated Main File

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <vector>
+#include "vector_int.h"
 using namespace std;
 
+
 int main() {
-    cout << "Hello World" << endl;
+    int p = 17289;
+    int q = 2;
+
+    vector<int> x = int_to_vector(p);
+    vector<int> y = int_to_vector(q);
+
+    print(x/y);
+    cout << (p/q) << endl;
+
+    print(x%y);
+    cout << (p%q) << endl;
 
     return 0;
 }
